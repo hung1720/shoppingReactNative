f2fbb83aedc7603af9983039222a7fc8d24122ea
= <=>
2023-02-01 18:04:53 +0700

    part2

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "remixicon/fonts/remixicon.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import store from './redux/store';
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
